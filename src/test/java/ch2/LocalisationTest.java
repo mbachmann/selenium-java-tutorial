44ed9e4927443499fb14a9e64dd33bcd3493f845
mbachmann <m.bachmann@united.ch>
2025-07-31 07:10:22 +0200

    chapter 02 initial

---
+++ src/test/java/ch2/LocalisationTest.java
@@ -0,0 +1,26 @@
+package ch2;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
+
+import utils.AbstractTest;
+
+public class LocalisationTest extends AbstractTest {
+
+	@BeforeEach
+	void setup() {
+		super.setup("https://the-internet.herokuapp.com/login");
+	}
+
+	@Test
+	void findElements() {
+		WebElement username = driver.findElement(By.id("username"));
+		WebElement password = driver.findElement(By.id("password"));
+		WebElement loginButton = driver.findElement(By.cssSelector("button.radius"));
+
+		Assertions.assertTrue(username.isDisplayed());
+		Assertions.assertTrue(password.isDisplayed());
+		Assertions.assertTrue(loginButton.isDisplayed());
+	}
+
+}
