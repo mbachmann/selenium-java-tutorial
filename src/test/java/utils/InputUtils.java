a118b211bd9095a415aaf756b71c414e3fbae809
mbachmann <m.bachmann@united.ch>
2025-08-06 12:50:13 +0200

    sendkeys with ! is not working on mac due to a bug in Selenium. The workaround is a copy paste class with InputUtils

--- src/test/java/utils/InputUtils.java
+++ src/test/java/utils/InputUtils.java
@@ -9,11 +9,11 @@ import org.openqa.selenium.WebElement;
 public class InputUtils {
 
     public static void pasteText(WebDriver driver, WebElement element, String text) {
-        // Text in die Zwischenablage legen
+        // copy Text in Clipboard
         Toolkit.getDefaultToolkit().getSystemClipboard()
                 .setContents(new StringSelection(text), null);
 
-        // Element fokussieren und einfügen
+        // Focus to Element and paste
         element.click();
         element.sendKeys(Keys.COMMAND, "v"); // CMD + V auf macOS
     }
