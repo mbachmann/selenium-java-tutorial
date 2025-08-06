7b33c5e96d8bd1dd2054f96c7ccd629126ae3223
mbachmann <m.bachmann@united.ch>
2025-08-06 11:30:20 +0200

    add allure, logback, new drivers, change readme

---
+++ src/test/java/ch01/task3/DuckDuckGoSearchPage.java
@@ -0,0 +1,24 @@
+package ch01.task3;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.*;
+
+import utils.HasLogger;
+
+public class DuckDuckGoSearchPage implements HasLogger {
+	private WebDriver driver;
+
+	@FindBy(id = "searchbox_input")
+	private WebElement searchInput;
+
+	public DuckDuckGoSearchPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+
+	public void search(String query) {
+		searchInput.sendKeys(query);
+		searchInput.sendKeys(Keys.ENTER);
+	}
+
+}
---
+++ src/test/java/ch01/task3/DuckDuckGoTest.java
@@ -0,0 +1,40 @@
+package ch01.task3;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.*;
+
+import utils.TestBase;
+
+public class DuckDuckGoTest extends TestBase {
+
+	private DuckDuckGoSearchPage page;
+
+
+	@BeforeEach
+	void setup() {
+		super.setup("https://duckduckgo.com");
+		page = new DuckDuckGoSearchPage(driver);
+	}
+
+	@Test
+	void testDuckDuckGoSearch() {
+
+		WebElement input = wait.until(ExpectedConditions.elementToBeClickable(By.id("searchbox_input")));
+		input.sendKeys("Selenium WebDriver");
+		input.sendKeys(Keys.ENTER);
+
+		wait.until(ExpectedConditions.titleContains("Selenium WebDriver"));
+		Assertions.assertTrue(driver.getTitle().contains("Selenium WebDriver"));
+	}
+
+	@Test
+	void testDuckDuckGoSearchWithPage() {
+
+		page.search("Selenium WebDriver");
+		wait.until(ExpectedConditions.titleContains("Selenium WebDriver"));
+		Assertions.assertTrue(driver.getTitle().contains("Selenium WebDriver"));
+	}
+
+
+}
