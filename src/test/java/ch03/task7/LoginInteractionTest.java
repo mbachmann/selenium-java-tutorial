0ef069c4b25887012d7fb28a8db8d3b8b69562bc
mbachmann <m.bachmann@united.ch>
2025-08-06 16:01:22 +0200

    refactor TestBase

--- src/test/java/ch03/task7/LoginInteractionTest.java
+++ src/test/java/ch03/task7/LoginInteractionTest.java
@@ -19,10 +19,9 @@ public class LoginInteractionTest extends TestBase implements HasLogger {
 	void loginTest() {
 		driver.findElement(By.id("username")).sendKeys("tomsmith");
 
-		// Copy password to clipboard
 		// sendkeys of ! on macos not working due to bug in selenium
 		WebElement pwd = driver.findElement(By.id("password"));
-		setValue( pwd,"SuperSecretPassword!");
+		TestBase.setValue(driver, pwd,"SuperSecretPassword!");
 
 		getLogger().info(driver.findElement(By.id("username")).getAttribute("value")); 		// is correct
 		getLogger().info(driver.findElement(By.id("username")).getDomAttribute("value"));		// is not correct (null)
