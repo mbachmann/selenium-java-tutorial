1e19181cc217514048c55c3ab82c8843065b4aaa
mbachmann <m.bachmann@united.ch>
2025-08-07 10:51:53 +0200

    add allure maven with current report version

--- src/test/java/utils/TestBase.java
+++ src/test/java/utils/TestBase.java
@@ -127,25 +127,28 @@ public class TestBase implements AfterTestExecutionCallback, HasLogger {
 		ch.qos.logback.classic.Logger root = (ch.qos.logback.classic.Logger) org.slf4j.LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME);
 		MapAppender mapAppender = (MapAppender)root.getAppender("map");
 
-		Map<String, ILoggingEvent> eventMap = mapAppender.getEventMap();
-		eventMap.forEach((k, event) -> {
-			logEntries.add(mapAppender.createLogEntry(event));
-		});
+		if (mapAppender != null) {
+			Map<String, ILoggingEvent> eventMap = mapAppender.getEventMap();
+			eventMap.forEach((k, event) -> {
+				logEntries.add(mapAppender.createLogEntry(event));
+			});
 
-		List<String> logs = new ArrayList<>();
-		logEntries.forEach(entry -> {
-			if (entry.contains("[browser]")) {
-				if (entry.contains("\n") && entry.contains("\r")) {
-					entry = entry.replace("\r", "");
+			List<String> logs = new ArrayList<>();
+			logEntries.forEach(entry -> {
+				if (entry.contains("[browser]")) {
+					if (entry.contains("\n") && entry.contains("\r")) {
+						entry = entry.replace("\r", "");
+					}
+					logs.addAll(Arrays.asList(entry.split("\n")));
+				} else {
+					logs.add(entry); // + System.lineSeparator());
 				}
-				logs.addAll(Arrays.asList(entry.split("\n")));
-			} else {
-				logs.add(entry); // + System.lineSeparator());
-			}
-		});
-		logs.sort(String::compareTo);
-		Allure.addAttachment("log", String.join("\n", logs));
-		eventMap.clear();
+			});
+			logs.sort(String::compareTo);
+			Allure.addAttachment("log", String.join("\n", logs));
+			eventMap.clear();
+		}
+
 	}
 
 	/**
@@ -154,7 +157,7 @@ public class TestBase implements AfterTestExecutionCallback, HasLogger {
 	 * @param element input element
 	 * @param value the value to set
 	 */
-	protected void setValue(WebElement element, String value) {
+	public static void setValue(WebDriver driver,  WebElement element, String value) {
 		try {
 			// First try to set the value using sendKeys)
 			element.clear();
@@ -167,7 +170,7 @@ public class TestBase implements AfterTestExecutionCallback, HasLogger {
 
 		} catch (Exception e) {
 			// Fallback to JavaScript if sendKeys fails or does not set the value correctly
-			((JavascriptExecutor) this.driver).executeScript(
+			((JavascriptExecutor) driver).executeScript(
 					"arguments[0].value = arguments[1];" +
 							"arguments[0].dispatchEvent(new Event('input', { bubbles: true }));" +
 							"arguments[0].dispatchEvent(new Event('change', { bubbles: true }));",
