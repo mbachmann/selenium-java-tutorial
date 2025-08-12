6ce9520ae456576e2cf4bb2f44ebdc86af9b3898
mbachmann <m.bachmann@united.ch>
2025-08-12 14:07:11 +0200

    change TestBase for working with Surefire

--- src/test/java/utils/TestBase.java
+++ src/test/java/utils/TestBase.java
@@ -32,8 +32,8 @@ public class TestBase implements AfterTestExecutionCallback, HasLogger {
 
 	protected void setup(String url) {
 		// driver = DriverFactoryExtended.initDriver("chrome", "http://localhost:4444/wd/hub");
-		driver = DriverFactoryExtended.initDriver("firefox", "");
-		// driver = DriverFactoryExtended.initDriver();
+		// driver = DriverFactoryExtended.initDriver("firefox", "");
+		driver = DriverFactoryExtended.initDriver();
 		DOWNLOAD_DIR = DriverFactoryExtended.getDownloadDir();
 		robustGet(driver, url);
 		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
